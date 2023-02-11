7e0f9165bfd7a24f8db214b89b61ec225110fe8c
Fay almagati <104152519+FayHussain@users.noreply.github.com>
2023-02-11 15:34:37 +0300

    Update Photoscript.js

--- Photoscript.js
+++ Photoscript.js
@@ -1,7 +1,7 @@
 function total(){
     let days = document.getElementById("days").value;
 
-  let myfunction = (a) => a*300;
+  let myfunction = (a) => a*300; //arrow function ES6
   let total2 = myfunction(days);
 
     document.getElementById("Total").innerHTML = " Your Total is "+total2+"R";
